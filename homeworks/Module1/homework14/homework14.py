876965a5e890b9ba29ccf323e71a78c28f483d58
makso <maksim.demin100@gmail.com>
2024-03-28 12:25:06 +0300

    homework14 added

---
+++ homeworks/Module1/homework14/homework14.py
@@ -0,0 +1,4 @@
+def print_hello():
+    print("Hello, world")
+def print_hello_name():
+    print("Hello, World")
---
+++ homeworks/Module1/homework14/main.py
@@ -0,0 +1,3 @@
+import homework14 as printf14
+printf14.print_hello()
+printf14.print_hello_name()
