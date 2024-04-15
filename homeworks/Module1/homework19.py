83a45b165003713d2bbb6d6d88f1b3b71b25be7e
makso <maksim.demin100@gmail.com>
2024-04-15 20:34:25 +0300

    homework19 update added

--- homeworks/Module1/homework19.py
+++ homeworks/Module1/homework19.py
@@ -1,12 +1,13 @@
 class Building:
-    total = 0
+    total = 0 #Создайте новый класс Buiding с атрибутом total
     def __init__(self):
-        Building.total += 1
+        Building.total += 1 #Создайте инициализатор для класса Buiding,
+        # который будет увеличивать атрибут количества созданных объектов класса Building total
 
 Building_massive = []
-while len(Building_massive) < 40:
+while len(Building_massive) < 40: #В цикле создайте 40 объектов класса Building и выведите их на экран командой print
     new_build = Building()
     Building_massive.append(new_build)
 for x in Building_massive:
     print(x)
-print(len(Building_massive))
+print(Building.total)
