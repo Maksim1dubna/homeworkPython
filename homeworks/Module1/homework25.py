aafb42701e35a77766685cf1a8a1d44ca45bfe1f
makso <maksim.demin100@gmail.com>
2024-04-30 18:03:45 +0300

    homework25.1 update added

--- homeworks/Module1/homework25.py
+++ homeworks/Module1/homework25.py
@@ -1,10 +1,13 @@
 import os
+import time
 directory = 'D:\\Steam\\steamapps\\workshop\\content\\784150'
 for dirpath, dirnames, filenames in os.walk(directory):
     print(dirpath, dirnames, filenames)
     print(os.path.dirname(directory))
     for file in filenames:
         full_name_path = dirpath + '\\' + file
-        print(os.path.getmtime(full_name_path))
+        filetime = os.path.getmtime(full_name_path)
+        formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
+        print(formatted_time)
         print(os.path.getsize(full_name_path))
         print(os.path.join(full_name_path))
