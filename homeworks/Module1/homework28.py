849fb663ae1f2d759924ad5d79831b970eac9b8b
makso <maksim.demin100@gmail.com>
2024-05-10 21:08:53 +0300

    homework28 update added

--- homeworks/Module1/homework28.py
+++ homeworks/Module1/homework28.py
@@ -1,13 +1,16 @@
 import warnings
 def divide_numbers(a, b):
+    warnings.simplefilter('ignore')
     try:
-        a / b
-        if b < 0.01:
+        if b <= 0.01:
             warnings.warn("Ты близок к 0....")
+            warnings.simplefilter("always")
+            warnings.warn("А теперь есть предупреждения...")
+            warnings.simplefilter("error")
+            warnings.warn("Но нет! Это ОШИБКА!")
+        return a / b
     except ZeroDivisionError:
         print("Я тебя предупреждал...")
     except UserWarning as e:
         print(e)
-
-
 print(divide_numbers(1,0.001))
