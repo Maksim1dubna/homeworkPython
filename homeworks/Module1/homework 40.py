6254ec8fd81849dae2573c406cfb04d67e627134
Maksim1dubna <68699608+Maksim1dubna@users.noreply.github.com>
2024-06-16 11:41:36 +0300

    homework40 update added

---
+++ homeworks/Module1/homework 40.py
@@ -0,0 +1,18 @@
+# Верните словарь или строки с данными об объекте, включающий следующую информацию:
+#    - Тип объекта.
+#    - Атрибуты объекта.
+#    - Методы объекта.
+#    - Модуль, к которому объект принадлежит.
+#    - Другие интересные свойства объекта, учитывая его тип (по желанию).
+import pprint
+import sys
+class turn_onOff:
+    def __init__(self):
+        self.sw = {"Включено":1, "Выключено":0}
+    def switch(self):
+        print(self.sw["Включено"])
+def ntrospection_info(obj):
+    pprint.pprint(f"{vars(obj)}, {type(obj)}")
+
+obj1 = turn_onOff
+ntrospection_info(obj1)
