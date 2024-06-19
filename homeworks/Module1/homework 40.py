ac1db2b3d2f6da1a7e5cda5c5c041c10e3ab2106
Maksim1dubna <68699608+Maksim1dubna@users.noreply.github.com>
2024-06-19 19:49:37 +0300

    homework40.1 update added

--- homeworks/Module1/homework 40.py
+++ homeworks/Module1/homework 40.py
@@ -4,15 +4,17 @@
 #    - Методы объекта.
 #    - Модуль, к которому объект принадлежит.
 #    - Другие интересные свойства объекта, учитывая его тип (по желанию).
+import inspect
 import pprint
 import sys
 class turn_onOff:
+    human = "Человек"
     def __init__(self):
         self.sw = {"Включено":1, "Выключено":0}
     def switch(self):
         print(self.sw["Включено"])
 def ntrospection_info(obj):
-    pprint.pprint(f"{vars(obj)}, {type(obj)}")
+    pprint.pprint(f"Type:{type(obj)}, methods and attributes:{[method for method in dir(obj) if method.startswith('__') is False]}, module:{inspect.getmodule(obj)}")
 
 obj1 = turn_onOff
 ntrospection_info(obj1)
