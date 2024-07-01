a64fc2d0723a3880b8853f4edd75951eef5af410
Maksim1dubna <68699608+Maksim1dubna@users.noreply.github.com>
2024-07-01 20:58:39 +0300

    Add files via upload

--- loghomework/main.py
+++ loghomework/main.py
@@ -1,42 +1,53 @@
-import requests as rq
 import logging
+import requests as rq
 
-logger200 = logging.getLogger('RequestsLogger')
-loggerNo200 = logging.getLogger('RequestsLoggerError')
-loggerNoResp = logging.getLogger('RequestsLoggerResponse')
+#Настройка логгеров
+success_logger = logging.getLogger('success_responses')
+success_logger.setLevel(logging.INFO) # Иначе не пишется в лог, можно поставить DEBUG
+bad_responses_logger = logging.getLogger('bad_responses')
+blocked_responses_logger = logging.getLogger('blocked_responses')
 
-sites = ['https://www.youtube.com/', 'https://instagram.com', 'https://wikipedia.org', 'https://yahoo.com',
-         'https://yandex.ru', 'https://whatsapp.com', 'https://twitter.com', 'https://amazon.com', 'https://tiktok.com',
-         'https://www.ozon.ru']
-logger200.setLevel(logging.INFO)
-loggerNo200.setLevel(logging.ERROR)
-loggerNoResp.setLevel(logging.WARNING)
+#Форматирование лог-сообщений
+formatter = logging.Formatter('%(levelname)s: %(message)s')
 
-logger200hand = logging.FileHandler("success_responses.log", mode='w')
-logger200form = logging.Formatter("%(name)s %(asctime)s %(levelname)s %(message)s")
-logger200hand.setFormatter(logger200form)
-logger200.addHandler(logger200hand)
+# Обработчики логов
+success_file_handler = logging.FileHandler('success_responses.log')
+bad_responses_file_handler = logging.FileHandler('bad_responses.log')
+blocked_responses_file_handler = logging.FileHandler('blocked_responses.log')
 
-loggerNo200hand = logging.FileHandler("bad_responses.log", mode='w')
-loggerNo200form = logging.Formatter("%(name)s %(asctime)s %(levelname)s %(message)s")
-loggerNo200hand.setFormatter(loggerNo200form)
-loggerNo200.addHandler(loggerNo200hand)
+# Добавление форматеров и обработчиков к логгерам
+success_logger.addHandler(success_file_handler)
+bad_responses_logger.addHandler(bad_responses_file_handler)
+blocked_responses_logger.addHandler(blocked_responses_file_handler)
 
-loggerNoResphand = logging.FileHandler("blocked_responses.log", mode='w')
-loggerNoRespform = logging.Formatter("%(name)s %(asctime)s %(levelname)s %(message)s")
-loggerNoResphand.setFormatter(loggerNoRespform)
-loggerNoResp.addHandler(loggerNoResphand)
+success_file_handler.setFormatter(formatter)
+bad_responses_file_handler.setFormatter(formatter)
+blocked_responses_file_handler.setFormatter(formatter)
 
+# Список сайтов
+sites = [
+    'https://www.youtube.com/',
+    'https://wikipedia.org',
+    'https://yahoo.com',
+    'https://yandex.ru',
+    'https://whatsapp.com',
+    'https://amazon.com',
+    'https://www.ozon.ru',
+    'https://instagram.com',
+    'https://twitter.com',
+]
+
+# Проверка доступности сайтов
 for site in sites:
     try:
         response = rq.get(site, timeout=3)
+
         if response.status_code == 200:
-            logger200.info(response)
-    except Exception:
-        if rq.exceptions.ReadTimeout:
-           print("Time out")
-           loggerNoResp.warning(response)
-           pass
-        loggerNo200.error(response)
-        pass
-    print(response)
+            success_logger.info(f'{site}, response - {response.status_code}')
+        else:
+            bad_responses_logger.warning(f'{site}, response - {response.status_code}')
+
+    except rq.exceptions.ConnectionError:
+        blocked_responses_logger.error(f'{site}, NO CONNECTION')
+    except (rq.exceptions.Timeout, rq.exceptions.HTTPError) as e:
+        bad_responses_logger.error(f'{site}, response - {e}')
