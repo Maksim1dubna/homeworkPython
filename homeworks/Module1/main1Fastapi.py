25c67b257fca36782776de8b809630f9bf8ebee1
Maksim1dubna <68699608+Maksim1dubna@users.noreply.github.com>
2024-07-31 19:16:31 +0300

    fastapi update added

--- homeworks/Module1/main1Fastapi.py
+++ homeworks/Module1/main1Fastapi.py
@@ -1,6 +1,6 @@
 #Для запуска python -m uvicorn main:app
 # Swagger /docs
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
 app = FastAPI()
 @app.get("/")
 async def main_page():
@@ -11,13 +11,16 @@ async def admin_page():
     return "Вы вошли как администратор"
 
 @app.get("/user/{user_id}")
-async def user_page(user_id):
+async def user_page(user_id: int = Path(ge = 1, le = 100, description = "Enter User ID", example= "93")):
     return f"Вы вошли как пользователь № {user_id}"
+#http://127.0.0.1:8000/user/100
 
-@app.get("/user")
-async def user_age_page(username: str, age: int):
+@app.get("/user/{username}/{age}")
+async def user_age_page(username: str = Path(min_length = 5, max_length = 20, description="Enter username", example = "UrbanUser"),
+                        age: int = Path(ge = 18, le = 120, description="Enter age", example = "24")):
     return f"Информация о пользователе. Имя: {username}, Возраст: {age}"
-    #http://127.0.0.1:8000/user?username=user&age=25
 
+    # http://127.0.0.1:8000/user/UrbanUser/24
+    #http://127.0.0.1:8000/user?username=user&age=25 для другой вариации
 
 
